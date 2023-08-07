e6aacecb0f321a6d6cb0d4253ba13860bbafb5d2
PileLiu <309829291@qq.com>
2023-08-08 00:05:30 +0800

    主页添加内容

--- src/views/childViews/IndexView.vue
+++ src/views/childViews/IndexView.vue
@@ -23,17 +23,98 @@
                 </div>
             </div>
             <div class="wave-next">
-                <i class="el-icon-arrow-down"></i>
+                <i @click="waveNext()" class="el-icon-arrow-down"></i>
             </div>
-            下滑
-            <el-button @click="getImg()">获取图片</el-button>
+            <div style="position: relative;top: 50px;">
+                <el-container>
+                    <el-aside width="400px" style="position: relative;left: 150px;">
+                        <div style="width: 300px;position: relative;top: -30px;">
+                            <!-- 名片 -->
+                            <el-card class="box-card box-card1" shadow="always">
+                                <div style="text-align: center;">
+                                    <span><img class="card-img" width="100px" height="100px"
+                                            src="@/assets/images/defaul.jpg" alt=""
+                                            style="border-radius: 50%;border: 1px solid #030303;"></span>
+                                    <h2 style="font-size: 30px;">Bibin</h2>
+                                </div>
+                                <div style="text-align: center;margin-top: -10px;">
+                                    <div style="float: left;position: relative;left: 20px;margin-left:30px">
+                                        <h4>文章</h4>
+                                        0
+                                    </div>
+                                    <div style="float: left;position: relative;left: 20px;margin-left:30px">
+                                        <h4>分类</h4>
+                                        0
+                                    </div>
+                                    <div style="float: left;position: relative;left: 20px;margin-left:30px">
+                                        <h4>访问量</h4>
+                                        0
+                                    </div>
+                                </div>
+                            </el-card>
+
+                            <!-- 搜索框 -->
+                            <el-card class="box-card box-card2" shadow="always">
+                                <span style="position: relative;top: -10px;font-weight: 900;">搜搜</span>
+                                <el-input placeholder="请输入内容" v-model="input2">
+                                </el-input>
+                                <i style="position: absolute;top: 50px;right: 35px;font-size: 20px;cursor: pointer;"
+                                    @click="search()" class="el-icon-search"></i>
+                            </el-card>
+                            <!-- 推荐文章 -->
+                            <el-card class="box-card box-card1" shadow="always">
+                                <span style="font-weight: 900;position: relative;"><i
+                                        class="el-icon-reading"></i>&nbsp;推荐文章</span>
+                                <el-empty style="position: relative;top: -30px;" description="博主暂无推荐"></el-empty>
+                            </el-card>
+                            <!-- 赞赏 -->
+                            <el-card class="box-card box-card3" shadow="always">
+                                <span style="font-weight: 900;"><i class="el-icon-chicken"></i>&nbsp;赞赏</span>
+                                <el-empty style="position: relative;top: -60px;" description="无任何记录"></el-empty>
+                            </el-card>
+
+
+                            <!-- 文章分类 -->
+                            <el-card class="box-card box-card3" shadow="always">
+                                <span style="font-weight: 900;"><i class="el-icon-location-outline"></i>&nbsp;标签</span>
+                                <el-empty style="position: relative;top: -60px;" description="暂无标签"></el-empty>
+                            </el-card>
+
+
+
+                        </div>
+                    </el-aside>
+                    <el-main style="position: relative;left: 200px;">
+                        <div style="width: 60%;margin-top: -20px;margin-left: 70px;">
+                            <el-card style="height: 300px;">
+
+                            </el-card>
+                            <el-card style="height: 300px;margin-top: 40px;">
+
+                            </el-card>
+                            <el-card style="height: 300px;margin-top: 40px;">
+
+                            </el-card>
+                            <el-card style="height: 300px;margin-top: 40px;">
+
+                            </el-card>
+                            <el-card style="height: 300px;margin-top: 40px;">
+
+                            </el-card>
+                        </div>
+                    </el-main>
+                </el-container>
+            </div>
+            <!-- 用来撑起高度 -->
+            <div class="content-after" style="height: 60px;">
+
+            </div>
         </div>
 
     </div>
 </template>
 
 <script>
-import userApi from '@/api/userApi'
 import ImgApi from '@/api/backgroundApi'
 export default {
     data() {
@@ -46,23 +127,38 @@ export default {
         }
     },
     methods: {
-        logout() {
-            userApi.getUser().then(response => {
-                console.log(response.data);
-            })
-        },
         getImg() {
             ImgApi.getImg().then(response => {
                 const randomIndex = Math.floor(Math.random() * response.data.length);
                 this.userImg = response.data[randomIndex].url;
             })
+        },
+        // 页面下滑
+        waveNext() {
+            const scrollDuration = 400; // 滚动总时间（毫秒）
+            const scrollDistance = window.innerHeight + 4; // 滚动距离
+            const startTime = performance.now(); // 开始时间
+            const scrollStep = timestamp => {
+                const currentTime = timestamp - startTime;
+                const progress = Math.min(currentTime / scrollDuration, 1);
+
+                window.scrollTo(0, progress * scrollDistance);
+                if (currentTime < scrollDuration) {
+                    requestAnimationFrame(scrollStep);
+                }
+            };
+            requestAnimationFrame(scrollStep);
+        },
+        // 搜索
+        search() {
+            console.log(123);
         }
     },
     mounted() {
         this.getImg();
         this.token = localStorage.getItem('token');
-        console.log("index:" + this.token);
         this.showImg = true;
+
     }
 }
 </script>
@@ -93,8 +189,8 @@ export default {
     left: 50%;
     text-align: center;
     color: black;
-    font-size: 45px;
-    font-weight: 900;
+    font-size: 50px;
+    font-weight: bolder;
     cursor: pointer;
     animation: waveNext 3.5s linear infinite;
 }
@@ -107,10 +203,12 @@ export default {
     50% {
         transform: translateY(-10px);
     }
+
     100% {
         transform: translateY(10px);
     }
 }
+
 .wave1,
 .wave2 {
     float: left;
@@ -122,40 +220,94 @@ export default {
 }
 
 .wave1 {
-    background-color: rgba(255, 255, 255, 0.9);
-    animation: waveAnimation 3.5s linear infinite;
+    background-color: rgba(255, 255, 255, 1);
+    animation: waveAnimation 5s linear infinite;
 }
 
 .wave2 {
     position: relative;
     left: -85px;
     background-color: rgba(250, 250, 250, 0.4);
-    animation: waveAnimation 2s linear infinite;
+    animation: waveAnimation 3s linear infinite;
 }
 
 @keyframes waveAnimation {
     0% {
-        transform: translateX(0) translateY(5px) rotate(5deg);
+        transform: translateX(0) translateY(0px) rotate(0deg);
         /* 调整波浪的高度 */
     }
 
     25% {
-        transform: translateX(-30%) translateY(5px) rotate(45deg);
+        transform: translateX(-25%) translateY(10px) rotate(90deg);
     }
 
     50% {
-        transform: translateX(-50%) translateY(-5px) rotate(90deg);
+        transform: translateX(-50%) translateY(0px) rotate(180deg);
         /* 调整波浪的高度 */
     }
 
     75% {
-        transform: translateX(-75%) translateY(0px) rotate(45deg);
+        transform: translateX(-75%) translateY(15px) rotate(270deg);
     }
 
     100% {
-        transform: translateX(-100%) translateY(3px) rotate(5deg);
+        transform: translateX(-100%) translateY(0px) rotate(360deg);
         ;
         /* 调整波浪的高度 */
     }
+}
+
+.box-card {
+    position: relative;
+    margin-top: 40px;
+    box-shadow: 2px -2px 5px rgba(0, 0, 0, 0.5) !important;
+}
+
+.box-card:hover {
+    transform: scale(1.01);
+}
+
+.card-img:hover {
+    cursor: pointer;
+    transition: 0.5s;
+    rotate: 360deg;
+}
+
+.box-card1 {
+    height: 300px;
+    background-size: 200% 200%;
+    background-image: linear-gradient(70deg, #29b6f6, #8bceee, #0aa6f5, #82c9f0);
+    animation: colorChange 5s linear infinite;
+}
+
+/* 定义动画效果 */
+@keyframes colorChange {
+    0% {
+        background-position: 0%;
+    }
+
+    25% {
+        background-position: 50%;
+    }
+
+    50% {
+        background-position: 100%;
+    }
+
+    75% {
+        background-position: 50%;
+    }
+
+    100% {
+        background-position: 0%;
+    }
+}
+
+.box-card2 {
+    height: 100px;
+}
+
+.box-card3 {
+    height: 250px;
 }
 </style>
