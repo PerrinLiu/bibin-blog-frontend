ceb0d329d2371a85c5afb94c031e79b7867520dc
LLPY <309829291@qq.com>
2024-06-09 23:09:36 +0800

    feat: 完成文章页面的布局

--- src/views/childViews/ArticleView.vue
+++ src/views/childViews/ArticleView.vue
@@ -11,33 +11,79 @@
             </div>
         </div>
 
-        <div class="content" style="margin-top: 50vh;">
-            <div style="height: 1000px;">
-                <WangEditorView></WangEditorView>
-            </div>
-            <!-- 用来撑起高度 -->
-            <div class="content-after" style="height: 60px;">
+        <div class="content item center" style="margin-top: 50vh;">
+            <div class="content-inner">
+                <div class="center">
+                    <el-input v-model="searchVo.title" placeholder="请输入内容" suffix-icon="el-icon-search"
+                        style="width: 50%;"></el-input>
+                </div>
+                <div>
+                    <el-row>
+                        <el-col :span="8" v-for="(o) in  12 " :key="o">
+                            <el-card :body-style="{ padding: '0px' }" style="margin: 20px;">
+                                <img width="100%" height="250px" style="cursor: pointer;"
+                                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                                    class="image" @click="goDetail()">
+                                <div style="padding: 20px;">
+                                    <span>好吃的汉堡</span>
+                                    <div style="margin-top: 10px;position: relative;">
+                                        <span style="font-size: 16px;color: #ccc;">2020-10-10</span>
+                                        <span style="font-size: 16px;position: absolute;right: 0px;">
+                                            <i class="el-icon-star-on">&nbsp;6</i>&nbsp;&nbsp;
+                                            <i class="el-icon-view">&nbsp;952</i>&nbsp;&nbsp;
+                                            <i class="el-icon-s-comment">&nbsp;23</i>
+                                        </span>
+                                    </div>
+                                    <div style="margin-top: 10px">
+                                        <el-tag style="margin-right:16px" v-for="item in 3" :key="item">
+                                            标签{{ item }}
+                                        </el-tag>
+                                    </div>
+                                </div>
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </div>
+                <div style="margin-top:20px;display: flex;justify-content: center;">
+                    <el-pagination background layout="prev, pager, next" :pageSize="searchVo.pageSize"
+                        :currentPage="searchVo.pageNum" :total="total" @current-change="changePage">
+                    </el-pagination>
+                </div>
+                <!-- 用来撑起高度 -->
+                <div class=" content-after" style="height: 60px;">
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import WangEditorView from '../components/WangEditorView';
 export default {
-    components: {
-        WangEditorView
-    },
     data() {
         return {
-            showImg: false
+            showImg: false,
+            searchVo: {
+                pageSize: 15,
+                pageNum: 1,
+                title: "",
+            },
+            total: 50,
         }
     },
     mounted() {
         this.showImg = true;
     },
     methods: {
-
+        goDetail() {
+            console.log("点击了卡片, 跳转到详情页");
+        },
+        changePage(val) {
+            this.searchVo.pageNum = val;
+            this.getArticle();
+        },
+        getArticle() {
+            console.log("搜索文章", this.searchVo);
+        }
     }
 }
 </script>
@@ -46,5 +92,16 @@ export default {
 .el-zoom-in-top-enter-active,
 .el-zoom-in-top-leave-active {
     transition: transform 1.2s;
+}
+
+.content-inner {
+    width: 70%;
+    min-width: 1024px;
+    margin-top: 50px;
+}
+
+.center {
+    display: flex;
+    justify-content: center;
 }
 </style>
